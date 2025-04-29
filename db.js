b7eb70f3c4c04efad86ad1945bcd1ad72c2d89a2
emiliano0807 <emilianorivfacio@gmail.com>
2025-04-28 22:56:05 -0600

    agregar configuración de la DB

---
+++ db.js
@@ -0,0 +1,27 @@
+// db.js
+const sql = require('mssql');
+require('dotenv').config();
+
+const config = {
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  server: process.env.SQL_SERVER,
+  database: process.env.SQL_DATABASE,
+  options: {
+    encrypt: process.env.SQL_ENCRYPT === 'true',
+    trustServerCertificate: process.env.SQL_TRUST === 'true'
+  }
+};
+
+const poolPromise = sql.connect(config)
+  .then(pool => {
+    console.log('Conectado a SQL Server');
+    return pool;
+  })
+  .catch(err => {
+    console.error('Error de conexión a SQL Server:', err);
+  });
+
+module.exports = {
+  sql, poolPromise
+};
